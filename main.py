a64deadd464c0e2e4453be4c3a4d93c88a81e3f1
hszadows <48970794+hszadows@users.noreply.github.com>
2022-09-11 19:47:21 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+# This is my project for Assignment 0
+
+answer = input("Did I understand the assignment? y/n: ").lower()
+
+if answer == "y":
+    print('yay')
+elif answer == 'n':
+    print('oh nooooo')
+else:
+    print('yikes')
+
